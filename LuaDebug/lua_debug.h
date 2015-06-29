246c7cec7316880aa5cedd61d797597a6d11f9c4
Jon Wayne <Jon_Wayne@163.com>
2015-06-29 23:15:42 +0800

    head file path fix

--- LuaDebug/lua_debug.h
+++ LuaDebug/lua_debug.h
@@ -2,11 +2,11 @@
 #define _LUA_DEBUG_H_
 
 #include <stdio.h>
-#include "lua\lua.h"
-#include "lua\lobject.h"
-#include "lua\lauxlib.h"
-#include "lua\lualib.h"
-#include "lua\lstate.h"
+#include "lua/lua.h"
+#include "lua/lobject.h"
+#include "lua/lauxlib.h"
+#include "lua/lualib.h"
+#include "lua/lstate.h"
 
 extern void ld_init(lua_State *L);
 extern void ld_printstack(lua_State *L);
